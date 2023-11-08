aa9e9d1ccd9508ebf425b754fd924f7e89636b8b
Ho Nyeong <dev.honing@gmail.com>
2023-11-08 23:05:19 +0900

    Fix: 기본 배열 arr = [1, 2, 3]으로 변경
    - 문제#2에 배열의 요소를 삭제하는 문제가 있어서 기본 배열의 요소를 변경하게 되었다.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const arr = [];
+const arr = [1, 2, 3];
 
 // Q1. 배열의 끝에 하나 이상의 요소를 추가하고, 배열의 새 길이를 반환하는 메서드는 무엇인가요?
   // Q1-1. 배열의 끝에 하나 이상의 요소를 추가하는 메서드? //* arr.push()
@@ -6,12 +6,12 @@ const arr = [];
   
   // A1-1. arr.push(): 배열 끝에 요소를 추가하는 메서드
   // A1-1-1. 숫자 추가
-  arr.push(0); 
-  console.log(arr); // [0]
+  arr.push(4); 
+  console.log(arr); // [1, 2, 3, 4]
 
   // A1-1-2. 문자열 추가
   arr.push("문자열"); 
-  console.log(arr); // [0, '문자열']
+  console.log(arr); // [1, 2, 3, 4, '문자열']
 
   // A1-2. arr.length: 배열의 길이를 반환하는 메서드
-  console.log(arr.length); // 2
+  console.log(arr.length); // 5

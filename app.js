bfe1eb128833581d87de81dcc496ebbc1da06673
Ho Nyeong <dev.honing@gmail.com>
2023-11-08 23:38:01 +0900

    문제#3 정답 작성
    - arr.shift(): 배열의 첫 번째 요소를 제거

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const arr = [1, 2, 3];
 
 // Q3. 배열에서 첫 번째 요소를 제거하고 그 요소를 반환하는 메서드는 무엇인가요?
 // A3. arr.shift(): 배열의 첫 번째 요소를 제거
+const firstElement = arr.shift();
+console.log(arr); // [2, 3]
+console.log(firstElement); // 1

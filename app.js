45d0b29c7e7f22f4ce8a6649490ea71d250c9cdb
Ho Nyeong <dev.honing@gmail.com>
2023-11-13 22:05:11 +0900

    문제#6 정답 작성

--- app.js
+++ app.js
@@ -4,11 +4,4 @@ const arr = [1, 2, 3];
 
 // A6. arr.splice(): 배열의 요소를 삭제, 교체, 추가가 가능
 arr.splice(1, 1, 4, 5); // 배열의 인덱스 1을 삭제하고, 4와 5를 추가하기
-// console.log(arr);
-
-// 추가학습
-const fruits = ['사과', '바나나', '오렌지', '키위'];
-
-// 인덱스 0부터 3개의 요소를 삭제하기
-fruits.splice(0, 3);
-console.log(fruits); // ['키위']
+console.log(arr);

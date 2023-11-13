aec33421b0e08577ab69d3f56932070910a957b2
Ho Nyeong <dev.honing@gmail.com>
2023-11-13 21:58:30 +0900

    문제#6 추가학습
    - 배열의 요소 삭제

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const arr = [1, 2, 3];
 
 // A6. arr.splice(): 배열의 요소를 삭제, 교체, 추가가 가능
 arr.splice(1, 1, 4, 5); // 배열의 인덱스 1을 삭제하고, 4와 5를 추가하기
-console.log(arr);
+// console.log(arr);
 
 // 추가학습
 const fruits = ['사과', '바나나', '오렌지', '키위'];
 
-// 인덱스부터 1에서 2개의 요소를 삭제하고, '체리'와 '수박'을 추가해보기'
-const fruitsSpliced = fruits.splice(1, 2, '체리', '수박');
-console.log(fruitsSpliced); // ['바나나', '오렌지']
-console.log(fruits); ['사과', '체리', '수박', '키위'];
+// 인덱스 2부터 끝까지 요소를 삭제해보기
+fruits.splice(2);
+console.log(fruits); // ['사과', '바나나']
